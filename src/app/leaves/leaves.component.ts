2faeac22347428d79e3822e1e7f03fbd85deb2f0
KOUSSONDA <kouskefil@gmail.com>
2025-10-27 00:47:36 +0100

    commit initial

---
+++ src/app/leaves/leaves.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaves',
+  imports: [],
+  templateUrl: './leaves.component.html',
+  styleUrl: './leaves.component.scss'
+})
+export class LeavesComponent {
+
+}
---
+++ src/app/volunteering/volunteering.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteering',
+  imports: [],
+  templateUrl: './volunteering.component.html',
+  styleUrl: './volunteering.component.scss'
+})
+export class VolunteeringComponent {
+
+}
